0fa16b1422dedd264d33d6eab6501feb65e14cb8
Yoshi <316977040+yoshiii-xyz@users.noreply.github.com>
2026-09-05 11:49:15 -0500

    Format and lint httpheader source

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,28 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
     let url = args.url;
-    let host = url.split("://").last().unwrap_or(&url).split('/').next().unwrap_or(&url);
-    let path = if let Some(p) = url.find(host) { &url[p + host.len()..] } else { "/" };
-    let port = if host.contains(":") { 80 } else { 80 };
+    let host = url
+        .split("://")
+        .last()
+        .unwrap_or(&url)
+        .split('/')
+        .next()
+        .unwrap_or(&url);
+    let path = if let Some(p) = url.find(host) {
+        &url[p + host.len()..]
+    } else {
+        "/"
+    };
+    let port = 80;
     let host = host.split(':').next().unwrap_or(host);
 
     let addr = format!("{}:{}", host, port);
     let mut stream = TcpStream::connect_timeout(&addr.parse()?, Duration::from_secs(10))?;
 
-    let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", path, host);
+    let request = format!(
+        "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
+        path, host
+    );
     stream.write_all(request.as_bytes())?;
 
     let mut buf = Vec::new();
